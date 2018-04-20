71c0bc7f22f18b7cb3c64d3c6ee691abe35fa4ce
Oscar Garzon <oscar.garzon@ciencias.unam.mx>
2018-04-20 13:20:19 -0500

    Métodos Get y Set listos. Método equals tambien

--- src/mx/unam/ciencias/icc/Carro.java
+++ src/mx/unam/ciencias/icc/Carro.java
@@ -19,110 +19,151 @@ public class Carro implements Registro {
     private String marca;
     /* Año del carro. */
     private int año;
-    /* Cilindraje del carro.*/
+    /* Cilindraje del carro. */
     private double cilindraje;
-    /*Caballos de fuerza del carro*/
+    /*Caballos de fuerza del carro. */
     private int caballosDeFuerza;
-    /*Torque del carro*/
+    /*Torque del carro. */
     private int torque;
 
     /**
-     * Construye un estudiante con todas sus propiedades.
-     * @param nombre el nombre del estudiante.
-     * @param cuenta el número de cuenta del estudiante.
-     * @param promedio el promedio del estudiante.
-     * @param edad la edad del estudiante.
+     * Construye un carro con todas sus propiedades.
+     * @param color el color del carro.
+     * @param marca la marca del carro.
+     * @param año el año del carro..
+     * @param cilindraje el cilindraje del carro.
+     * @param caballosDeFuerza los caballos de fuerza del carro.
+     * @param torque el torque del carro.
      */
-    public Estudiante(String nombre,
-                      int    cuenta,
-                      double promedio,
-                      int    edad) {
-        this.nombre = nombre;
-        this.cuenta = cuenta;
-        this.promedio = promedio;
-        this.edad = edad;
+    public Estudiante(String color,
+                      String marca,
+                      int    año,
+                      double cilindraje,
+                      int    caballosDeFuerza
+                      int torque) {
+        this.color = color;
+        this.marca = marca;
+        this.año = año;
+        this.cilindraje =  cilindraje;
+        this.caballosDeFuerza = caballosDeFuerza;
+        this.torque = torque;
     }
 
     /**
-     * Regresa el nombre del estudiante.
-     * @return el nombre del estudiante.
+     * Regresa el color del carro.
+     * @return el color del carro.
      */
-    public String getNombre() {
-        return nombre;
+    public String getColor() {
+        return color;
     }
 
     /**
-     * Define el nombre del estudiante.
-     * @param nombre el nuevo nombre del estudiante.
+     * Define el color del carro.
+     * @param color el nuevo color del carro.
      */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setColor(String color) {
+        this.color = color;
     }
 
     /**
-     * Regresa el número de cuenta del estudiante.
-     * @return el número de cuenta del estudiante.
+     * Regresa la marca del carro.
+     * @return la marca del carro.
      */
-    public int getCuenta() {
-        return cuenta;
+    public String getMarca() {
+        return marca;
     }
 
     /**
-     * Define el número cuenta del estudiante.
-     * @param cuenta el nuevo número de cuenta del estudiante.
+     * Define la marca del carro.
+     * @param marca la nueva marca del carro.
      */
-    public void setCuenta(int cuenta) {
-        this.cuenta = cuenta;
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
 
     /**
-     * Regresa el promedio del estudiante.
-     * @return el promedio del estudiante.
+     * Regresa el año del carro.
+     * @return el año del carro.
      */
-    public double getPromedio() {
-        return promedio;
+    public int getAño() {
+        return año;
     }
 
     /**
-     * Define el promedio del estudiante.
-     * @param promedio el nuevo promedio del estudiante.
+     * Define el año del carro.
+     * @param año el nuevo año del carro.
      */
-    public void setPromedio(double promedio) {
-        this.promedio = promedio;
+    public void setAño(int año) {
+        this.año = año;
     }
 
     /**
-     * Regresa la edad del estudiante.
-     * @return la edad del estudiante.
+     * Regresa el cilindraje del carro.
+     * @return el cilindraje del carro.
      */
-    public int getEdad() {
-        return edad;
+    public double getCilindraje() {
+        return cilindraje;
     }
 
     /**
-     * Define la edad del estudiante.
-     * @param edad la nueva edad del estudiante.
+     * Define el cilindraje del carro.
+     * @param cilindraje el nuevo cilindraje del carro.
      */
-    public void setEdad(int edad) {
-        this.edad = edad;
+    public void setCilindraje(double cilindraje) {
+        this.cilindraje = cilindraje;
     }
 
     /**
-     * Nos dice si el objeto recibido es un estudiante igual al que manda llamar
+     * Regresa los caballos de fuerza del carro.
+     * @return los caballos de fuerza del carro.
+     */
+    public int getCaballosDeFuerza() {
+        return caballosDeFuerza;
+     }
+
+    /**
+     *  Define los caballos de fuerza del carro.
+     * @param caballosDeFuerza los nuevos caballos de fuerza del carro.
+     */
+    public void setCaballosDeFuerza(int caballosDeFuerza) {
+        this.caballosDeFuerza = caballosDeFuerza;
+     }
+
+    /**
+     * Regresa el torque del carro.
+     * @return el torque del carro.
+     */
+    public int getTorque() {
+          return torque;
+     }
+
+    /**
+     * Define el torque del carro.
+     * @param torque el nuevo torque del carro.
+    */
+    public void setTorque(int torque) {
+        this.torque = torque;
+    }
+
+
+    /**
+     * Nos dice si el objeto recibido es un carro igual al que manda llamar
      * el método.
-     * @param o el objeto con el que el estudiante se comparará.
-     * @return <tt>true</tt> si el objeto o es un estudiante con las mismas
+     * @param o el objeto con el que el carro se comparará.
+     * @return <tt>true</tt> si el objeto o es un carro con las mismas
      *         propiedades que el objeto que manda llamar al método,
      *         <tt>false</tt> en otro caso.
      */
     @Override public boolean equals(Object o) {
-        if (!(o instanceof Estudiante))
+        if (!(o instanceof Carro))
             return false;
-        Estudiante e = (Estudiante)o;
-        if (this.nombre.equals(e.getNombre()) &&
-            this.edad == e.getEdad()     &&
-            this.cuenta == e.getCuenta() &&
-            this.promedio == e.getPromedio())
+        Carro c = (Carro)o;
+        if (this.color.equals(c.getColor()) &&
+        this.marca.equals(c.getMarca()) &&
+        this.año == c.getAño(); &&
+        this.cilindraje == e.getCilindraje() &&
+        this.caballosDeFuerza == e.getCaballosDeFuerza() &&
+        this.torque == e.getTorque())
                 return true;
         return false;
     }
